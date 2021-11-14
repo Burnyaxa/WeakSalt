79a476458fb1583a8fb15e260d68f5f606c6794b
Artem Supryhan <suprigan.artem1@gmail.com>
2021-11-14 23:46:03 +0200

    Init

---
+++ WeakSalt/Extensions/ByteExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WeakSalt.Extensions
+{
+    public static class ByteExtensions
+    {
+        public static IEnumerable<byte> Xor(this IEnumerable<byte> first, IEnumerable<byte> second)
+        {
+            var firstLength = first.Count();
+            var secondLength = second.Count();
+            var result = new List<byte>();
+
+            for (int i = 0; i < firstLength; i++)
+            {
+                result.Add((byte)(first.ElementAt(i) ^ second.ElementAt(i % secondLength)));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WeakSalt/Extensions/StringExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace WeakSalt.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToHex(this string str)
+        {
+            return str.Select(x => Convert.ToString(x, 16)).Aggregate("", (s, s1) => s + s1);
+        }
+
+        public static string FromHex(this string str)
+        {
+            var result = new StringBuilder();
+            var bufferBuilder = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                bufferBuilder.Append(str[i]);
+                if (bufferBuilder.Length == 2)
+                {
+                    result.Append((char)Convert.ToByte(bufferBuilder.ToString(), 16));
+                    bufferBuilder.Clear();
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public static string XorHexStrings(this string str1, string str2)
+        {
+            var val1 = BigInteger.Parse(str1, NumberStyles.HexNumber);
+            var val2 = BigInteger.Parse(str2, NumberStyles.HexNumber);
+            var result = val1 ^ val2;
+            var resultString = result.ToString("X");
+            return resultString.Length % 2 == 0 ? resultString : resultString + "0";
+        }
+
+        public static IEnumerable<byte> HexToByteEnumerable(this string str)
+        {
+            var result = new List<byte>();
+          
+            for (var i = 0; i < str.Length; i += 2)
+            {
+                var hexStr = str.Substring(i, 2);
+                result.Add(Convert.ToByte(hexStr, 16));
+            }
+
+            return result;
+        }
+    }
+}
